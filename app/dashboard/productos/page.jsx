93691e6ab44978822bdedacb238bb789ae7ffcdc
restoker <milthon.doom@gmail.com>
2025-02-09 21:25:43 -0500

    any commit

--- app/dashboard/productos/page.jsx
+++ app/dashboard/productos/page.jsx
@@ -1,5 +1,18 @@
+import { db } from "@/server";
 
-export default function ProductosPage() {
+export default async function ProductosPage() {
+    const productos = await db.query.products.findMany({
+        orderBy: (products, { desc }) => desc(products.updated),
+        with: {
+            productImages: {
+                columns: {
+                    key: true,
+                    url: true,
+                }
+            }
+        }
+    });
+    console.log(productos);
     return (
         <div>
             <h1 className="text-white">Productos</h1>
